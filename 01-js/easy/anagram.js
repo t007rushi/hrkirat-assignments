8cfcc3581e446bef1b599c643cc9046d69af1683
Rushikesh Tarapure <rushikeshtarapure@gofynd.com>
2024-01-17 23:38:11 +0530

    Solved Easy/Anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,12 @@
 */
 
 function isAnagram(str1, str2) {
-
+if(str1.toLowerCase().split("").sort().join() === str2.toLowerCase().split("").sort().join()) {
+  // console.log(str1.split("").toLowerCase().sort().join(),str2.toLowerCase().split("").sort().join())
+  return true
+}else {
+  // console.log(str1.split("").sort().join(),str2.split("").sort().join())
+  return false}
 }
-
+isAnagram('Debit Card', 'Bad Credit');
 module.exports = isAnagram;
